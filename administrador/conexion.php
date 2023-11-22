69f73729066c3a09a76a6df7a7e6ab353957cf60
maglio-quiroga <magliocquirogaf@gmail.com>
2023-11-22 14:04:17 -0300

    conexion a la base de datos lista

---
+++ administrador/conexion.php
@@ -0,0 +1,13 @@
+<?php
+
+$server="localhost";
+$user="root";
+$password="maglio100";
+$db="herramientas-desarrollo2";
+
+$conexion = new mysqli($server,$user,$pass,$db);
+$conexion->set_charset("utf-8");
+
+
+
+?>
