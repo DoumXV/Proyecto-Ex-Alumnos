05270bd0294bb36c2137f55a485e7f516b52a2d7
maglio-quiroga <magliocquirogaf@gmail.com>
2023-11-22 17:00:30 -0300

    avanzado el inicio de sesion, hay problemas a la hora de ejecutar la consulta sql de validacion

--- administrador/conexion.php
+++ administrador/conexion.php
@@ -6,7 +6,7 @@ $password="maglio100";
 $db="herramientas-desarrollo2";
 
 $conexion = new mysqli($server,$user,$pass,$db);
-$conexion->set_charset("utf-8");
+
 
 
 
--- administrador/inicio-sesion.php
+++ administrador/inicio-sesion.php
@@ -1,23 +1,30 @@
 <?php
-session_start();
+$server="localhost";
+$user="root";
+$password="maglio100";
+$db="herramientas-desarrollo2";
+
+$conexion = new mysqli($server,$user,$pass,$db);
+
+if($conexion){
+    echo "conexion exitosa";
+}
+
 if(isset($_POST['boton_enviar'])){
     if (!empty($_POST['email_admin']) and !empty($_POST['clave_admin'])) {
         $email=$_POST['email_admin'];
         $clave=$_POST['clave_admin'];
-        $sql=$conexion->query(" select * from admin where email_admin='$email' and clave_admin='$clave'");
-        if ($datos=$sql->fetch_object()) {
-            $_SESSION=$datos["nombre_admin"];
-            $_SESSION=$datos["email_admin"];
-            $_SESSION=$datos["clave_admin"];
-            header("Location:gestion.php");
-        } else {
-            echo "Datos incorrectos";
-        }
-        
-    } else {
-        echo "No se pueden dejar campos vacios";
+        echo $email.$clave;
+
     }
+    else{
+        echo "No se pueden ingresar campos vacios";
+    }
+
+
+        
     
 }
+
 
 ?>
