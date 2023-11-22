52acaccabd712ac820792df678dc0f645feb083d
maglio-quiroga <magliocquirogaf@gmail.com>
2023-11-22 18:42:35 -0300

    backend inicio de sesesion, cierre de sesion,validacion comentada para las otras paginas

--- administrador/inicio-sesion.php
+++ administrador/inicio-sesion.php
@@ -1,4 +1,6 @@
 <?php
+session_start();
+
 $server="localhost";
 $user="root";
 $password="maglio100";
@@ -6,23 +8,30 @@ $db="herramientas-desarrollo2";
 
 $conexion = new mysqli($server,$user,$pass,$db);
 
-if($conexion){
-    echo "conexion exitosa";
-}
 
 if(isset($_POST['boton_enviar'])){
     if (!empty($_POST['email_admin']) and !empty($_POST['clave_admin'])) {
         $email=$_POST['email_admin'];
         $clave=$_POST['clave_admin'];
-        echo $email.$clave;
-
+        $sql=$conexion->query("SELECT * FROM administrador WHERE email_admin='$email' AND clave_admin='$clave'");
+        #$sql=$conexion->query("SELECT * FROM `administrador` WHERE email_admin=\'maglio@gmail.com\' AND clave_admin=\'maglio\'");
+        if ($datos=$sql->fetch_object()) {
+            $_SESSION['email_admin']=$datos->email_admin;
+            $_SESSION['clave_admin']=$datos->clave_admin;
+            $_SESSION['nombre_admin']=$datos->nombre_admin;
+            #header();
+        } else {
+            echo "Datos incorrectos";
+        }
+        
     }
     else{
         echo "No se pueden ingresar campos vacios";
     }
-
-
-        
+    #para las otras paginas es esta validacion
+    #<?php
+    #session_start();
+    #if(empty($_SESSION['email_admin'])){ header(); }  
     
 }
 
