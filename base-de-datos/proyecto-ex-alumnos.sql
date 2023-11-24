e408074d242cdaae8d4f0fbe1f319c99a86c5ea3
Doublas <doumxv@gmail.com>
2023-11-24 20:28:37 -0300

    Base de Datos Oficial

--- base-de-datos/proyecto-ex-alumnos.sql
+++ base-de-datos/proyecto-ex-alumnos.sql
@@ -1,14 +1,13 @@
 -- phpMyAdmin SQL Dump
--- version 4.9.1
+-- version 5.2.1
 -- https://www.phpmyadmin.net/
 --
--- Servidor: localhost
--- Tiempo de generación: 24-11-2023 a las 00:46:15
--- Versión del servidor: 8.0.17
--- Versión de PHP: 7.3.10
+-- Servidor: 127.0.0.1
+-- Tiempo de generación: 25-11-2023 a las 00:27:11
+-- Versión del servidor: 10.4.28-MariaDB
+-- Versión de PHP: 8.2.4
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET AUTOCOMMIT = 0;
 START TRANSACTION;
 SET time_zone = "+00:00";
 
@@ -19,7 +18,7 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Base de datos: `herramientas-desarrollo2`
+-- Base de datos: `proyecto-ex-alumnos`
 --
 
 -- --------------------------------------------------------
@@ -32,7 +31,7 @@ CREATE TABLE `administrador` (
   `nombre_admin` varchar(200) NOT NULL,
   `email_admin` varchar(200) NOT NULL,
   `clave_admin` varchar(200) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Volcado de datos para la tabla `administrador`
@@ -48,11 +47,13 @@ INSERT INTO `administrador` (`nombre_admin`, `email_admin`, `clave_admin`) VALUE
 --
 
 CREATE TABLE `empleos` (
+  `id_empleo` int(11) NOT NULL,
   `titulo` varchar(200) NOT NULL,
   `empresa` varchar(200) NOT NULL,
   `ciudad` varchar(200) NOT NULL,
-  `descripcion` varchar(200) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+  `descripcion` varchar(2000) NOT NULL,
+  `archivo` varchar(5000) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
 
@@ -61,12 +62,26 @@ CREATE TABLE `empleos` (
 --
 
 CREATE TABLE `eventos` (
-  `id` int(11) NOT NULL,
+  `id_evento` int(11) NOT NULL,
   `nombre_evento` varchar(200) NOT NULL,
-  `lugar` varchar(200) NOT NULL,
+  `ubicacion` varchar(200) NOT NULL,
   `inicio` datetime NOT NULL,
   `final` datetime NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `peticiones`
+--
+
+CREATE TABLE `peticiones` (
+  `id_peticion` int(11) NOT NULL,
+  `nombre_peticion` varchar(200) NOT NULL,
+  `email_peticion` varchar(200) NOT NULL,
+  `contacto_peticion` varchar(200) NOT NULL,
+  `descripcion_peticion` varchar(2000) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
 
@@ -76,33 +91,70 @@ CREATE TABLE `eventos` (
 
 CREATE TABLE `usuarios` (
   `nombre_usuario` varchar(200) NOT NULL,
-  `fecha_egreso` varchar(200) NOT NULL,
+  `fecha_egreso` date NOT NULL,
   `area_interes` varchar(200) NOT NULL,
   `trabajo_actual` varchar(200) NOT NULL,
   `email_usuario` varchar(200) NOT NULL,
   `direccion_imagen` varchar(2000) NOT NULL,
+  `contacto` varchar(200) NOT NULL,
   `descripcion` varchar(2000) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Índices para tablas volcadas
 --
 
 --
+-- Indices de la tabla `administrador`
+--
+ALTER TABLE `administrador`
+  ADD PRIMARY KEY (`email_admin`);
+
+--
+-- Indices de la tabla `empleos`
+--
+ALTER TABLE `empleos`
+  ADD PRIMARY KEY (`id_empleo`);
+
+--
 -- Indices de la tabla `eventos`
 --
 ALTER TABLE `eventos`
-  ADD PRIMARY KEY (`id`);
+  ADD PRIMARY KEY (`id_evento`);
+
+--
+-- Indices de la tabla `peticiones`
+--
+ALTER TABLE `peticiones`
+  ADD PRIMARY KEY (`id_peticion`);
+
+--
+-- Indices de la tabla `usuarios`
+--
+ALTER TABLE `usuarios`
+  ADD PRIMARY KEY (`email_usuario`);
 
 --
 -- AUTO_INCREMENT de las tablas volcadas
 --
 
 --
+-- AUTO_INCREMENT de la tabla `empleos`
+--
+ALTER TABLE `empleos`
+  MODIFY `id_empleo` int(11) NOT NULL AUTO_INCREMENT;
+
+--
 -- AUTO_INCREMENT de la tabla `eventos`
 --
 ALTER TABLE `eventos`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id_evento` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `peticiones`
+--
+ALTER TABLE `peticiones`
+  MODIFY `id_peticion` int(11) NOT NULL AUTO_INCREMENT;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
