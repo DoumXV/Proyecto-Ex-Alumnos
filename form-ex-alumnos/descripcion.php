654d1368cd08d66ec9f3c0a53eb1995817c26681
maglio-quiroga <magliocquirogaf@gmail.com>
2023-11-25 23:28:47 -0300

    listo el codigo del formulario de ex-alumnos, falta la revision e integracion

--- form-ex-alumnos/descripcion.php
+++ form-ex-alumnos/descripcion.php
@@ -1,5 +1,28 @@
 <?php
 
+$server="localhost";
+$user="root";
+$password="";
+$db="proyecto-ex-alumnos";
+
+$conexion = new mysqli($server,$user,$pass,$db);
+
+$nombre=$_POST['nombre_peticion'];
+$email=$_POST['email_peticion'];
+$contacto=$_POST['contacto_peticion'];
+$descripcion=$_POST['descripcion_peticion'];
+
+if (!empty($nombre) and !empty($email) and !empty($contacto) and !empty($descripcion)) {
+    $sql1=$conexion->query("SELECT * FROM usuarios WHERE email_usuario='$email'");
+    if($sql1){
+        $sql2=$conexion->query("INSERT INTO peticiones (nombre_peticion,email_peticion,contacto_peticion,descripcion_peticion) VALUES ('$nombre','$email','$contacto','$descripcion')");
+        if($sql2){
+            echo "Solicitud enviada, su revision esta en proceso.";
+        }
+    }
+} else {
+    echo "No se pueden ingresar campos vacios";
+}
 
 
 ?>
