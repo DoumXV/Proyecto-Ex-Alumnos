622dba611b73f2548e6324ce9a74c5deee30ac6b
Vicente Jaramillo <vicente.jaramillo.contreras@gmail.com>
2023-12-09 21:48:00 -0300

    Empleos html listo

---
+++ empleos/empleos.php
@@ -0,0 +1,66 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Ex alumnos UDA</title>
+</head>
+<meta charset="UTF-8">
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+<link rel="stylesheet" href="empleos.css">
+<body>
+	<header>
+		<a href="../home/index.php" class="logo"><img class="logo-uda" src="../img/logo-corp-diic-txtblanco.png" alt="Logo UDA"></a>
+		<nav>
+			<ul>
+				<li><a class="linkeado" href="../home/index.php">Inicio</a></li>
+				<li><a class="linkeado" href="../galeria/galeria.php">Galeria</a></li>
+                <li><a class="linkeado" href="empleos.php">Empleos</a></li>
+				<li><a class="linkeado" href="../administrador/inicio-sesion.php">Administrador</a></li>
+			</ul>
+		</nav>
+	</header>
+
+    <section class="zona1"><div class="header-difuminado mx-auto"></div></section>
+
+    </section>
+
+    <section class="empleos">
+
+    </section>
+
+    <footer>
+        <div class="contenedor-footer">
+            <div class="footer-logo">
+                <img src="../img/logo-udacorp-txtblanco.png" alt="">
+                <ul>
+                  <li class="mt-1"><a href="">Ubícanos en
+                    Copiapó, Av. Copayapu 485</a></li>
+                  <li class="mt-1"><a href="">(52) 2 255555</a></li>
+                  <li class="mt-1"><a href="">anakarina.pena@uda.cl</a></li>
+              </ul>
+            </div>
+            <div class="acerca mx-auto">
+                <h4 class="mt-2">Links</h4>
+                <ul>
+                    <li class="mt-4"><a href="https://diicc.uda.cl/academicos.php">Académicos</a></li>
+                    <li class="mt-1"><a href="https://diicc.uda.cl/noticias.php">Noticias</a></li>
+                    <li class="mt-1"><a href="https://www.moodle.uda.cl/">Moodle</a></li>
+                    <li class="mt-1"><a href="https://www.instagram.com/diicc_uda/">Instagram</a></li>
+                </ul>
+            </div>
+            <div class="mapa">
+                <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1019.5733182946361!2d-70.35256776105918!3d-27.35752684276538!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x969803ec1a19a399%3A0x3cd60b166021c427!2sDIICC%20UDA!5e0!3m2!1ses-419!2scl!4v1700927546943!5m2!1ses-419!2scl" width="400" height="300" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+            </div>
+        </div>    
+        <div class="copyright">
+          <p>&copy;2023 Creado por alumnos de Ingeniería Civil en Computación e Informática 2023</p>
+      </div>
+    </footer>
+	<script type="text/javascript">
+		window.addEventListener("scroll", function(){
+			var header = document.querySelector("header");
+			header.classList.toggle("abajo",window.scrollY>0);
+		})
+	</script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
+</body>
+</html>
