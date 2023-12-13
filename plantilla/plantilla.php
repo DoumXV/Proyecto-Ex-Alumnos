99c5f4faeec80b3b1bff915ac84b12d3826e6fad
Maglio <magliocquirogaf@gmail.com>
2023-12-12 21:31:13 -0300

    preparacion para el merge

--- plantilla/plantilla.php
+++ plantilla/plantilla.php
@@ -13,7 +13,7 @@
 		<nav>
 			<ul>
 				<li><a class="linkeado" href="#">Inicio</a></li>
-				<li><a class="linkeado" href="../galeria/galeria.php">Galeria</a></li>
+				<li><a class="linkeado" href="../galeria/galeria.php">Galerias</a></li>
         <li><a class="linkeado" href="../empleos/empleos.php">Empleos</a></li>
 				<li><a class="linkeado" href="../log-admin/admin.php">Administrador</a></li>
 			</ul>
@@ -72,3 +72,4 @@
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
 </body>
 </html>
+
