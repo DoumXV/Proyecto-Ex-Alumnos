11bed5426f837a3ab97fcdbf6181c332d92bfe67
Maglio <magliocquirogaf@gmail.com>
2023-12-16 00:29:07 -0300

    mensaje arreglado

--- crud-alumnos/eliminar.php
+++ crud-alumnos/eliminar.php
@@ -3,9 +3,9 @@ if(!empty($_GET["email"])){
     $id_empleo=$_GET["email"];
     $sql=$conexion->query("DELETE FROM usuarios WHERE email_usuario='$id_empleo'");
     if ($sql==1) {
-        echo '<div class="text-center alert alert-success">Empleo eliminado correctamente</div>';
+        echo '<div class="text-center alert alert-success">Alumno eliminado correctamente</div>';
     } else {
-        echo '<div class="alert alert-danger">Error al eliminar empleo</div>';
+        echo '<div class="alert alert-danger">Error al eliminar el alumno</div>';
     }   
 }
 ?>
