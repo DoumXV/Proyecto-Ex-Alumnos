66e1e5c2c3d2a4b206eb15100d7293cd1843eecf
Maglio <magliocquirogaf@gmail.com>
2023-12-16 00:26:47 -0300

    ajuste de un mensaje

--- crud-empleos/eliminar-empleo.php
+++ crud-empleos/eliminar-empleo.php
@@ -3,9 +3,9 @@ if(!empty($_GET["id_empleo"])){
     $id_empleo=$_GET["id_empleo"];
     $sql=$conexion->query("DELETE FROM empleos WHERE id_empleo='$id_empleo'");
     if ($sql==1) {
-        echo '<div class="text-center alert alert-success">Empleo eliminado correctamente</div>';
+        echo '<div class="text-center alert alert-success">Alumno eliminado correctamente</div>';
     } else {
-        echo '<div class="alert alert-danger">Error al eliminar empleo</div>';
+        echo '<div class="alert alert-danger">Error al eliminar el alumno</div>';
     }   
 }
 ?>
